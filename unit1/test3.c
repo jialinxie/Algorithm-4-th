46547931c45cf0f0a402b2799bb823d0de04c055
jacklin <lin990033@gmail.com>
2017-03-09 23:09:19 +0800

    课后题1.3
    验证结果ok，方法比较简单

--- unit1/test3.c
+++ unit1/test3.c
@@ -1,45 +1,53 @@
-//只使用处理IO的printDigit函数，编写一个过程以输出任意实数（可以为负）
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
-void printDigit(int num){
-    printf("%d",num);
+void PrintDigit(int num){
+    printf("%d", num);
 }
-void printOut(int num){
-    if(num>10)
-        printOut(num/10);
-    printDigit(num%10);
-}
-//d:输入实数;decplace:舍入位置
-void printDouble(double d,int decPlace){
-    int num = d;
-    double dec = d-num;
-    if(num<0){
-        putchar('-');
-        num = -num;
-        dec = -dec;
-    }
-    printOut(num);
-    putchar('.');
-    
+
+void PrintOut(int num){
+    char a[100];
     int i;
-    for(i = 0;i<decPlace;i++){
-        dec = dec*10;
-        num = dec;
-        dec = dec-num;
-        if(i==decPlace-1){//打印最后一位时注意舍入
-            dec = dec*10;
-            int tmp = dec;
-            if(tmp>4)
-                num++;
-        }
-        printOut(num);
-        
+    
+    sprintf(a, "%d", num);
+    
+    for(i = 1; i < strlen(a); i++)
+      printf("%c", a[i]);
+}
+
+void PrintDouble(double num, int precision){
+    int numInt = 0;
+    double tail;
+    int tailInt = 0;
+    
+    
+    if(num < 0){
+        printf("-");
+        num = num - 2 * num;
+    }
+    
+    numInt = (int)num;
+    PrintDigit(numInt);
+    
+    tail = num - numInt;
+    if(tail > 0){    //double num
+        printf(".");
+        tailInt = tail * pow(10, precision);
+        PrintDigit(tailInt);
+        return;
+    }
+    if(tail == 0){  //int num
+        printf(".");
+        tailInt = 1 * pow(10, precision);
+        PrintOut(tailInt);
+        return;
     }
 }
 
-int main(){
-    printDouble(-9.1345,3);
-    printf("\nhello\n");
-    system("pause");
+void main(void)
+{
+    PrintDouble(-1.234233, 5);
+    printf("\n");
+    return;
 }
