6382e8453ffe0a148751790a8a82813b60b2a289
jacklin <lin990033@gmail.com>
2018-08-20 01:13:09 +0800

    update 1-2-1

--- Answer/java/Algorithm-1-2-1/src/com/company/Main.java
+++ Answer/java/Algorithm-1-2-1/src/com/company/Main.java
@@ -1,66 +1,61 @@
-import java.awt.geom.Point2D;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.DecimalFormat;
-import java.util.Random;
-
 /*
+*
+* 1.2.1编写一个Point2D的用例，从命令行接受一个整数N。在单位正方形内生成N个随机点，然后计算两点之
+* 间的最近距离。
+
 1.2.1 Write a Point2D client that takes an integer value N from the command line,
-generates N random points in the unit square, and computes the distance separating
-the closest pair of points.
-*/
+generates N random points in the unit square, and computes the distance
+separating the closest pair of points.
+*
+* */
 
-    public class Main {
-        private static int N;
-        private static Point2D[] pointsArr;
-        public static void main(String [] args) {
-            readInput();
-            generatePoints();
+import java.util.ArrayList;
+
+public class  Main{
+
+    public static class Point2D {
+
+        public double x;
+        public double y;
+
+        //初始化函数
+        public Point2D(double x, double y) {
+            this.x = x;
+            this.y = y;
         }
 
-        public static void generatePoints() {
-            Random rand = new Random();
-            Double minDist = Double.POSITIVE_INFINITY;
-            int a = 0;
-            int b = 0;
-            DecimalFormat df = new DecimalFormat("#.##");
-            for (int i=0; i<N; i++) {
-                double x = rand.nextDouble();
-                double y = rand.nextDouble();
-                pointsArr[i] = new Point2D();
-                pointsArr[i].setLocation(x, y);
-                for (int j=0; j<(i-1); j++) {
-                    double dist = pointsArr[i].distTo(pointsArr[j]);
-                    if (dist < minDist) {
-                        minDist = dist;
-                        a = i; b = j;
-                    }
-                }
-            }
-            printPoints();
-            System.out.println("closest points are: ");
-            System.out.print("(" + df.format(pointsArr[a].getX()) + "," + df.format(pointsArr[a].getY()) + ") and ");
-            System.out.println("(" + df.format(pointsArr[b].getX()) + "," + df.format(pointsArr[b].getY()) + ")");
-            System.out.println("min distance is: " + df.format(minDist));
-        }
-
-        public static void printPoints() {
-            int len = pointsArr.length;
-            DecimalFormat df = new DecimalFormat("#.##");
-            for (int i=0; i<len; i++) {
-                System.out.println("(" + df.format(pointsArr[i].getX()) + "," + df.format(pointsArr[i].getY()) + ")");
-            }
-        }
-
-        public static void readInput() {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            try {
-                N = Integer.parseInt(br.readLine());
-                pointsArr = new Point2D[N];
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        //计算两点之间的距离
+        public double distTo(Point2D that) {
+            double xDistance = this.x - that.x;
+            double yDistance = this.y - that.y;
+            return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
         }
     }
+
+
+    public static void main(String[] args){
+
+        int N = 10;
+        ArrayList<Point2D> points = new ArrayList<Point2D>();
+        //创建N个元素的数组
+        for (int i = 0; i < N; i++) {
+            Point2D point2d = new Point2D(Math.random(), Math.random());
+            points.add(point2d);
+        }
+        //计算第一个点到第二个点的距离
+        double resultDistince = points.get(0).distTo(points.get(1));
+
+        for (int i = 0; i < N; i++) {
+            Point2D point2d = points.get(i);
+            for (int j = i + 1; j < N; j++) {
+                //计算第i个点和第j个点之间的距离
+                double distince = point2d.distTo(points.get(j));
+                //如果距离比resultDistince小，就赋值给resultDistince
+                if (distince < resultDistince) {
+                    resultDistince = distince;
+                }
+            }
+        }
+        System.out.println("resultDistince:" + resultDistince);
+    }
+}
