1074ffd46b2319da052ffd159a19d80d4030ef18
jacklin <lin990033@gmail.com>
2018-08-29 00:18:23 +0800

    complete 1-2-19

---
+++ Answer/java/1-2-19/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/*
+* 1.2.19 字符串解析。为你在练习1.2.13中实现的Date和Transaction类型编写能够解析字符串数据的构造函数。
+* 他接受一个String参数指定的初始值，格式如表1.2.20所示：
+*
+* 1.2.19 Parsing. Develop the parse constructors for your Date and Transaction implementations
+ * of Exercise 1.2.13 that take a single String argument to specify the initialization values,
+ * using the formats given in the table below.
+ * Partial solution:
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //参考1.2.13, 已实现题目所需功能
+    }
+}
