7df83d17b0b73be55c99051d20a4c1a7042e66c4
jacklin <lin990033@gmail.com>
2018-10-18 14:30:14 +0800

    complete InfixToPosfix

--- cpp/Binomial.cpp
+++ cpp/Binomial.cpp
@@ -4,7 +4,12 @@
 #include "cmath"
 
 using namespace std;
-
+/*
+1.1.27     Binomial distribution. Estimate the number of recursive calls that would be used by the code public
+ static double binomial(int N, int k, double p) {    if ((N == 0) || (k < 0)) return 1.0;
+ return (1.0 - p)*binomial(N-1, k) + p*binomial(N-1, k-1); } to compute binomial(100, 50). Develop a better
+ implementation that is based on saving computed values in an array.
+*/
 double binomial1(int N, int k, double p, Counter &c){
     c++;
     if(N == 0 && k == 0)
---
+++ cpp/Stack.h
@@ -0,0 +1,130 @@
+/*
+ * 下压栈，链表实现<智能指针>
+ *
+ * */
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <memory>
+using std::shared_ptr;
+using std::make_shared;
+
+//提前声明
+template <typename T>
+class StackIterator;
+
+//链表的下压栈实现
+template <typename T>
+class Stack{
+	friend class StackIterator<T>;
+
+public:
+	//默认构造函数
+	Stack() = default;
+	//拷贝构造函数
+	Stack(const Stack<T> &);
+	//赋值构造函数
+	const Stack& operator = (const Stack<T>&);
+	bool isEmpty() const{
+		return first == nullptr;
+	}
+
+	int size() const{
+		return N;
+	}
+
+	//返回但不弹出栈顶元素
+	T peak() const{
+		return first->item;
+	}
+
+	void push(T item){
+		shared_ptr<Node> oldfirst = first;
+		first = make_shared<Node>();	//?
+		first->item = item;
+		first->next = oldfirst;
+		N++;
+	}
+
+	T pop(){
+		T item = first->item;
+		first = first->next;
+		N--;
+		return item;
+	}
+
+	//范围for 循环需要的end函数
+	StackIterator<T> end(){
+		return StackIterator<T>();
+	}
+
+	private:
+		//链表数据结构的嵌套累
+		class Node{
+		public:
+			Node() = default;
+			Node(Node &x){
+				item = x.item;
+				next = x.next;
+			}
+			T item;
+			shared_ptr<Node> next;
+		};
+
+		//拷贝栈
+		void copyStack(Stack &lhs, const Stack &rhs){
+			if(rhs.first != nullptr){
+				lhs.first = make_shared<Node>(*rhs.first);
+				for(shared_ptr<Node> x = lhs.first; x->next != nullptr; x = x->next)
+					x->next = make_shared<Node>(*(x->next));
+			}
+		}
+
+		shared_ptr<Node> first = nullptr;
+		int N = 0;
+};
+
+//栈的迭代器类
+template <typename T>
+class StackIterator
+{
+public:
+	StackIterator() = default;
+	StackIterator(shared_ptr<class Stack<T>::Node> &_first) : first(_first){}
+	~StackIterator();
+
+	//如果要求能执行范围for循环，则必须实现下面3个函数
+	const T& operator*() const{
+		return first->item;
+	}
+
+	StackIterator operator++(){
+		first = first->next;
+		return *this;
+	}
+
+	bool operator != (const StackIterator<T> &rhs) const{
+		return first != rhs.first;
+	}
+
+private:
+	//跟Stack中的first指向同样一块数据
+	shared_ptr<class Stack<T>::Node> first = nullptr;
+	
+};
+
+template <typename T>
+Stack<T>::Stack(const Stack &s){
+	if(s.first != nullptr)
+		copyStack(*this, s);
+}
+
+template <typename T>
+const Stack<T>& Stack<T>::operator=(const Stack &rhs){
+	if(rhs.first != nullptr)
+		copyStack(*this, rhs);
+	return *this;
+}
+
+#endif
